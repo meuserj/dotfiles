7dda00908ffe4cdd504025b8ec1b9fe7bca10b22
John Meuser <john.meuser@inin.com>
2017-08-16 15:41:39 -0400

    Switch to molokai

--- .shellprompt.sh
+++ .shellprompt.sh
@@ -4,7 +4,7 @@
 function __promptline_host {
   local only_if_ssh="0"
 
-  if [ ! $only_if_ssh -o -n "${SSH_CLIENT}" ]; then
+  if [ $only_if_ssh -eq 0 -o -n "${SSH_CLIENT}" ]; then
     if [[ -n ${ZSH_VERSION-} ]]; then print %m; elif [[ -n ${FISH_VERSION-} ]]; then hostname -s; else printf "%s" \\h; fi
   fi
 }
@@ -161,21 +161,21 @@ function __promptline {
   local alt_rsep=""
   local reset="${wrap}0${end_wrap}"
   local reset_bg="${wrap}49${end_wrap}"
-  local a_fg="${wrap}38;5;232${end_wrap}"
-  local a_bg="${wrap}48;5;144${end_wrap}"
-  local a_sep_fg="${wrap}38;5;144${end_wrap}"
-  local b_fg="${wrap}38;5;253${end_wrap}"
-  local b_bg="${wrap}48;5;16${end_wrap}"
-  local b_sep_fg="${wrap}38;5;16${end_wrap}"
-  local c_fg="${wrap}38;5;253${end_wrap}"
-  local c_bg="${wrap}48;5;67${end_wrap}"
-  local c_sep_fg="${wrap}38;5;67${end_wrap}"
+  local a_fg="${wrap}38;5;59${end_wrap}"
+  local a_bg="${wrap}48;5;3${end_wrap}"
+  local a_sep_fg="${wrap}38;5;3${end_wrap}"
+  local b_fg="${wrap}38;5;15${end_wrap}"
+  local b_bg="${wrap}48;5;59${end_wrap}"
+  local b_sep_fg="${wrap}38;5;59${end_wrap}"
+  local c_fg="${wrap}38;5;208${end_wrap}"
+  local c_bg="${wrap}48;5;59${end_wrap}"
+  local c_sep_fg="${wrap}38;5;59${end_wrap}"
   local warn_fg="${wrap}38;5;232${end_wrap}"
   local warn_bg="${wrap}48;5;166${end_wrap}"
   local warn_sep_fg="${wrap}38;5;166${end_wrap}"
-  local y_fg="${wrap}38;5;253${end_wrap}"
-  local y_bg="${wrap}48;5;16${end_wrap}"
-  local y_sep_fg="${wrap}38;5;16${end_wrap}"
+  local y_fg="${wrap}38;5;15${end_wrap}"
+  local y_bg="${wrap}48;5;59${end_wrap}"
+  local y_sep_fg="${wrap}38;5;59${end_wrap}"
   if [[ -n ${ZSH_VERSION-} ]]; then
     PROMPT="$(__promptline_left_prompt)"
     RPROMPT="$(__promptline_right_prompt)"
